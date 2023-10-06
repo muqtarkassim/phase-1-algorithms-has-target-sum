c94930de92fd01cbd562bcd1e2d251a358cc8f19
muqtar <muqtarabdiaziz8@gmail.com>
2023-10-06 14:17:42 +0300

    comp

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const newArray=[...array];
+  for(let i=0;i<newArray.length;i++){
+    const numberneeded=target-newArray[i];
+console.log(i)
+    console.log(`numberneeded ${numberneeded}`)
+    for(let j=i+1;j< newArray.length;j++){
+      //console.log(j)
+      if(newArray[j]===numberneeded){
+       // console.log('true')
+       return true
+      }
+    }
+   
+  }
+return false
 }
 
+//hasTargetSum([3, 8, 12, 4, 11, 7], 10)
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
   Add your pseudocode here
+  add the provided array into an new array
+
 */
 
-/*
-  Add written explanation of your solution here
-*/
+
+  //Add written explanation of your solution here
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
